8fbc1fe04f189955b5db79180c78217e323acfb9
Will Eardley <eardley.will@gmail.com>
2024-03-31 21:00:43 +0100

    Created JokeController

--- ASP.NET-FirstProject/Data/ApplicationDbContext.cs
+++ ASP.NET-FirstProject/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ASP.NET_FirstProject.Models;
 
 namespace ASP.NET_FirstProject.Data;
 
@@ -9,5 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<ASP.NET_FirstProject.Models.Joke>? Joke { get; set; }
 }
 
